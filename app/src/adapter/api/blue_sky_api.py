12d987e81aef06bf62556619dd84419428a230ca
Carlos Campos <carloscamposiki@gmail.com>
2025-07-29 20:17:45 -0300

    fix: posts mapping

--- app/src/adapter/api/blue_sky_api.py
+++ app/src/adapter/api/blue_sky_api.py
@@ -1,24 +1,11 @@
 import requests
 from src.exception.blue_sky_exception import BlueSkyException
-from src.entity.post import Post
 from src.adapter.api.token_manager import TokenManager
 
 class BlueSkyAPI:
 
     def __init__(self, token_generator: TokenManager):
         self.token_generator = token_generator
-
-    def get_user_posts(self, username: str, limit: int = 100) -> list[Post]:
-        url = f'https://bsky.social/xrpc/app.bsky.feed.getAuthorFeed?actor={username}&limit={limit}'
-        token = self.token_generator.get_session().get_token()
-        headers = {'Authorization': token}
-
-        response = requests.get(url, headers=headers)
-
-        if response.status_code == 200:
-            return self.map_posts(response.json()['feed'])
-        else:
-            raise BlueSkyException(f'Failed to fetch posts: {response.status_code} - {response.text}')
 
     def get_trends(self):
         url = 'https://bsky.social/xrpc/app.bsky.unspecced.getTrends'
@@ -44,12 +31,8 @@ class BlueSkyAPI:
         else:
             raise BlueSkyException(f'Failed to search posts: {response.status_code} - {response.text}')
 
-    def map_posts(self, response: map) -> list[Post]:
+    def map_posts(self, response: map) -> list[str]:
         posts = []
         for item in response:
-            post = Post(
-                cid=item['post']['cid'],
-                content=item['post']['record']['text']
-            )
-            posts.append(post)
+            posts.append(item['record']['text'])
         return posts
--- app/src/services/posts_fetcher.py
+++ app/src/services/posts_fetcher.py
@@ -10,13 +10,6 @@ class PostsFetcher:
     def fetch(self, new_trends: list[Trend]) -> dict[Trend, list[str]]:
         posts_content = {}
         for trend in new_trends:
-            search_response = self.blue_sky_api.search_posts(trend.name)
-            posts = self._extract_post_content(search_response)
+            posts = self.blue_sky_api.search_posts(trend.name)
             posts_content[trend] = posts
         return posts_content
-
-    def _extract_post_content(self, search_response: list[dict]) -> list[str]:
-        posts = []
-        for post in search_response:
-            posts.append(post['record']['text'])
-        return posts
